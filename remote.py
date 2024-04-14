23b73304da01af848e1a516fcbde43099e535a93
ritik222004 <151066849+ritik222004@users.noreply.github.com>
2024-04-14 23:12:32 +0530

    Add files via upload

---
+++ remote.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def run_parallel():
+    process1 = subprocess.Popen(['python', 'sw.py'])
+    process2 = subprocess.Popen(['python', 'deleteautomatic.py'])
+    process1.wait()
+    process2.wait()
+    
+if __name__ == "__main__":
+    run_parallel() 
